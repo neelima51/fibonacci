531b56c2dde3f39d1cbf701bc07519c69ee7ea2d
neelu <neelu@LAPTOP-602PTKRJ>
2016-11-03 17:59:00 -0400

    commit

---
+++ hibernateBasics/src/org/hibernate/tutorial/hbm/NativeApiIllustrationTest.java
@@ -0,0 +1,94 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package org.hibernate.tutorial.hbm;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+/**
+ * Illustrates use of Hibernate native APIs.
+ *
+ * @author Steve Ebersole
+ */
+public class NativeApiIllustrationTest {
+	
+	private static SessionFactory sessionFactory;
+
+	public static void main(String[] args) {
+		
+		/*
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure() // configures settings from hibernate.cfg.xml
+				.build();
+		try {
+			sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+		}
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+			// so destroy it manually.
+			
+			System.out.println("exception occurred while creating sessionfactory " + e.getMessage());
+			StandardServiceRegistryBuilder.destroy( registry );
+		}
+		*/
+		
+		sessionFactory= new Configuration().configure().buildSessionFactory();
+		
+		
+		
+		System.out.println("before creating session");
+		
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save( new Event( "Our very first event!", new Date() ) );
+		session.save( new Event( "A follow up event", new Date() ) );
+		session.save( new Event( "next event", new Date() ) );
+		session.getTransaction().commit();
+		session.close();
+	
+		System.out.println("saved the entity to database");
+		
+
+		// now lets pull events from the database and list them
+		session = sessionFactory.openSession();
+        session.beginTransaction();
+        List result = session.createQuery( "from Event" ).list();
+		for ( Event event : (List<Event>) result ) {
+			System.out.println( "Event (" + event.getDate() + ") : " + event.getTitle() );
+		}
+        session.getTransaction().commit();
+        session.close();
+
+	}
+
+}
